d9be83eb32f35fd7d61b355c8ce3c7a1157f8270
Mikkel <31762830+Mikkelens@users.noreply.github.com>
2021-08-17 17:35:22 +0200

    added the FlowControl project

---
+++ FlowControl/FlowControl/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Numerics;
+
+namespace FlowControl101
+{
+    class Program
+    {
+        public static Vector2Int printPos = new Vector2Int(5, 14);
+
+        static void Main(string[] args)
+        {
+            int spawnDiameter = 10;
+            int cooldown = -1;
+
+            Vector2Int xpPos = PlaceRandomXP(spawnDiameter);
+
+            // player
+            char headChar = 'p';
+            char bodyChar = 'o';
+            int bodyLenght = 1; // 1 is head only
+            Vector2Int headPos = new Vector2Int(0, 0);
+            Vector2Int[] currentPositions = new Vector2Int[] {};
+            Vector2Int[] oldPositions;
+            Console.SetCursorPosition(headPos.X, headPos.Y);
+            Console.Write(headChar);
+
+            ConsoleKey inputKey;
+            do // inputKey is not assigned yet
+            {
+                cooldown--;
+                oldPositions = currentPositions;
+
+                inputKey = Console.ReadKey(true).Key;
+                if (inputKey == ConsoleKey.RightArrow) headPos.X++;
+                else if (inputKey == ConsoleKey.LeftArrow && headPos.X > 0) headPos.X--;
+                if (inputKey == ConsoleKey.DownArrow) headPos.Y++;
+                else if (inputKey == ConsoleKey.UpArrow && headPos.Y > 0) headPos.Y--;
+
+                Console.SetCursorPosition(headPos.X, headPos.Y);
+                Console.Write(currentPositions[0]);
+
+                currentPositions = new Vector2Int[bodyLenght];
+                foreach (Vector2Int pos in currentPositions)
+                {
+                    //pos = oldPositions TODO: USE FOR LOOP INSTEAD FOR INDEX
+                }
+
+
+                // WIN STATES ETC
+                if (headPos.X == xpPos.X && headPos.Y == xpPos.Y)
+                {
+                    cooldown = 5;
+                    xpPos = PlaceRandomXP(spawnDiameter);
+                    ShowWin(true);
+                }
+                if (cooldown == 0) ShowWin(false);
+
+            } while (inputKey != ConsoleKey.Escape); // inputKey is assigned
+
+            Console.SetCursorPosition(printPos.X, printPos.Y);
+            Console.Write("Spillet afsluttes nu ... Tryk på en tast!");
+            Console.ReadKey();
+        }
+
+        static void UpdateCharacter()
+        {
+            
+        }
+
+        static void ShowWin(bool display)
+        {
+            if (display)
+            {
+                Console.SetCursorPosition(printPos.X, printPos.Y);
+                Console.Write("Du har samlet X op ...");
+            }
+            else
+            {
+                Console.SetCursorPosition(printPos.X, printPos.Y);
+                Console.Write("                      ");
+            }
+        }
+
+        static Vector2Int PlaceRandomXP(int maxRange)
+        {
+            Console.CursorVisible = false;
+            Random r = new Random();
+            Vector2Int xpPos = new Vector2Int(r.Next(0, maxRange + 1), r.Next(0, maxRange + 1));
+            Console.SetCursorPosition(xpPos.X,xpPos.Y);
+            Console.Write("X");
+            return xpPos;
+        }
+    }
+
+    [Serializable]
+    public struct Vector2Int
+    {
+        public int X;
+        public int Y;
+        
+        public Vector2Int(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ FlowControl/Program (BACKUP).cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FlowControl101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int cooldown = -1;
+
+            Console.CursorVisible = false;
+
+            Console.SetCursorPosition(15, 7);
+            Console.Write("X");
+
+            ConsoleKey brian;
+
+            do
+            {
+                cooldown--;
+
+                Console.SetCursorPosition(x, y);
+                Console.Write("P");
+
+                brian = Console.ReadKey(true).Key;
+
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+
+                if (brian == ConsoleKey.RightArrow)
+                {
+                    x++;
+                }
+                if (brian == ConsoleKey.DownArrow)
+                {
+                    y++;
+                }
+
+                if (x == 15 && y == 7)
+                {
+                    Console.SetCursorPosition(5, 14);
+                    Console.Write("Du har samlet X op ...");
+                    cooldown = 5;
+                }
+
+                if (cooldown == 0)
+                {
+                    Console.SetCursorPosition(5, 14);
+                    Console.Write("                      ");
+                }
+
+            } while (brian != ConsoleKey.Escape);
+
+            Console.SetCursorPosition(5, 14);
+            Console.Write("Spillet afsluttes nu ... Tryk på en tast!");
+            Console.ReadKey();
+        }
+    }
+}
