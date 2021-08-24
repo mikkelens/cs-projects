29097c288cb9b769634ff3b71e32292e85f3a4d1
Mikkelen1 <Mikkel123sej@gmail.com>
2021-08-24 16:46:52 +0200

    added FlowToCode

---
+++ FlowToCode/FlowToCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FlowToCode
+{
+    class Program
+    {
+        // PROGRAM //
+        static void Main(string[] args)
+        {
+            PrintNumbers();
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+        // ASSIGNMENT //
+        static void PrintNumbers()
+        {
+            Console.WriteLine("--- ALL NUMBERS ---");
+            for (int number = 1; number < 10; number++)
+            {
+                if (number != 3 && number != 5) // number *isnt* 3 AND *isnt* 5
+                    Console.WriteLine($"Number: {number}.");
+                else // number *is* 3 OR is 5
+                    Console.WriteLine($"BANNED NUMBER. ({number})");
+            }
+            Console.WriteLine("--- END ---");
+        }
+    }
+}
