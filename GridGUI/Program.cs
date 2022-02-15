89d0836b25350590ef6f3ca021071c4e10a7d940
Mikkel <31762830+Mikkelens@users.noreply.github.com>
2022-02-15 01:42:05 +0100

    ?

---
+++ GridGUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"PRINT: {new Vector2(1, 4).ToIndex()}");
+        End();
+    }
+    static void End()
+    {
+        Console.CursorVisible = false;
+        Console.Write("End of program.");
+        Console.ReadKey();
+    }
+}
---
+++ GridGUI/Tools.cs
@@ -0,0 +1,6 @@
+﻿using System.Numerics;
+
+public static class Tools
+{
+    public static int ToIndex(this Vector2 v) => default;
+}
