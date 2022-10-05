34c345d5bfbc595163514b44cef0b4f4ccdb2c82
Mikkel <Mikkel123sej@gmail.com>
2022-10-05 14:07:44 +0200

    run everything on program run

--- Polymorphism_0/Program.cs
+++ Polymorphism_0/Program.cs
@@ -4,12 +4,12 @@ internal static class Program
 {
 	public static void Main()
 	{
-		// Examples0.Run();
-		// Console.Write(NewLine(3));
-		// Assignments0.Run();
+		Examples0.Run();
+		Console.Write(NewLine(3));
+		Assignments0.Run();
 
-		// Examples1.Run();
-		// Console.Write(NewLine(3));
+		Examples1.Run();
+		Console.Write(NewLine(3));
 		Assignments1.Run();
 
 		string NewLine(int amount = 1) => new string('\n', amount);
