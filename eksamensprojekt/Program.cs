9117d7306143e55080ff0c72b6e3d594a379e275
Mikkel <Mikkel123sej@gmail.com>
2023-03-07 13:28:16 +0100

    added new project: eksamensprojekt

---
+++ eksamensprojekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+	string json = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+	Console.Write(json);
+}
